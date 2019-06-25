4cfe29709d84a56512b503e14e01cee27b046fb1
JadenGu <247760733@qq.com>
2019-06-25 10:23:25 +0800

    Remove unused menu initialization.

--- app/src/main/java/com/test/photobooth/MainActivity.kt
+++ app/src/main/java/com/test/photobooth/MainActivity.kt
@@ -7,8 +7,6 @@ import android.os.Environment
 import android.provider.MediaStore
 import android.text.TextUtils
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
@@ -62,22 +60,6 @@ class MainActivity : AppCompatActivity() {
             // update the data of adapter
             adapter.setPhotos(it)
         })
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     // create image file
